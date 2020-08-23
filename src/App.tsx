6c3a064b58921cc11267e77764d81df62e626426
Jimmy Cuadra <jimmy@jimmycuadra.com>
2020-08-23 09:21:39 -0700

    Use the correct incantation to let TypeScript load YAML.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import About from './About';
-import Albums from './Albums';
+import Albums, { Album } from './Albums';
 import Footer from './Footer';
 import Links from './Links';
 import Podcasts from './Podcasts';
@@ -17,7 +17,7 @@ export default function App() {
     <div className="container">
       <Header />
       <About />
-      <Albums albums={discography} />
+      <Albums albums={discography as unknown as Album[]} />
       <Links />
       <Podcasts />
       <Footer />
