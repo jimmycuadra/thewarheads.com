2c6795ff68f6aa18267a6c72ae50cd1e550974d1
Jimmy Cuadra <jimmy@jimmycuadra.com>
2020-08-31 10:25:35 -0700

    Don't wrap contents on album pages.

--- src/components/Album.tsx
+++ src/components/Album.tsx
@@ -23,65 +23,69 @@ export default function Album(props: AlbumProps) {
 
         <p>Released {albumModel.monthName} {albumModel.day}, {albumModel.year}</p>
 
-        <img
-          src={`/images/${albumModel.imageSlug}.jpg`}
-          alt={albumModel.title}
-          title={albumModel.title}
-          className="album-art"
-        />
+        <div className="column">
+          { (albumModel.amazon || albumModel.apple) && <h2>Listen</h2> }
 
-        { (albumModel.amazon || albumModel.apple) && <h2>Listen</h2> }
-
-        {
-          albumModel.amazon &&
-          <p><a href={albumModel.amazon}>Buy on Amazon</a></p>
-        }
-
-        {
-          albumModel.apple &&
-          <p><a href={albumModel.apple}>Listen on Apple Music or buy on iTunes</a></p>
-        }
-
-        {
-          albumModel.credits &&
-          <React.Fragment>
-            <h2>Credits</h2>
-
-            <p>
-              {albumModel.credits.map(credit => <span>{credit}<br /></span>)}
-            </p>
-          </React.Fragment>
-        }
-
-        {
-          albumModel.musicians &&
-          <React.Fragment>
-            <h2>Musicians</h2>
-
-            <p>
-              {albumModel.musicians.map(musician => <span>{musician}<br /></span>)}
-            </p>
-          </React.Fragment>
-        }
-
-        <h2 className="clear">Track listing</h2>
-
-        <table>
-          <tr>
-            <th>Title</th>
-            <th>Time</th>
-          </tr>
           {
-            albumModel.tracks.map((track) => {
-              return (
-                <tr>
-                  <td>{track.title}</td>
-                  <td>{track.time}</td>
-                </tr>
-              );
-            })
+            albumModel.amazon &&
+            <p><a href={albumModel.amazon}>Buy on Amazon</a></p>
           }
-        </table>
+
+          {
+            albumModel.apple &&
+            <p><a href={albumModel.apple}>Listen on Apple Music or buy on iTunes</a></p>
+          }
+
+          {
+            albumModel.credits &&
+            <React.Fragment>
+              <h2>Credits</h2>
+
+              <p>
+                {albumModel.credits.map(credit => <span>{credit}<br /></span>)}
+              </p>
+            </React.Fragment>
+          }
+
+          {
+            albumModel.musicians &&
+            <React.Fragment>
+              <h2>Musicians</h2>
+
+              <p>
+                {albumModel.musicians.map(musician => <span>{musician}<br /></span>)}
+              </p>
+            </React.Fragment>
+          }
+
+          <h2>Track listing</h2>
+
+          <table>
+            <tr>
+              <th>Title</th>
+              <th>Time</th>
+            </tr>
+            {
+              albumModel.tracks.map((track) => {
+                return (
+                  <tr>
+                    <td>{track.title}</td>
+                    <td>{track.time}</td>
+                  </tr>
+                );
+              })
+            }
+          </table>
+        </div>
+        <div className="album-art">
+          <img
+            src={`/images/${albumModel.imageSlug}.jpg`}
+            alt={albumModel.title}
+            title={albumModel.title}
+            className="album-art"
+          />
+        </div>
+        <div className="clear"></div>
       </React.Fragment>
     );
   }
